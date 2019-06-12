d4cc9bd75cb510afd6a432f874e19e8c28b8cdd7
Joop Ringelberg <joop.ringelberg@gmail.com>
2019-06-12 12:41:40 +0200

    Uncurried addAttachment

--- src/main.purs
+++ src/main.purs
@@ -3,12 +3,16 @@ module Main (addAttachment) where
 import Control.Promise (Promise, fromAff)
 import Data.MediaType (MediaType)
 import Effect (Effect)
+import Effect.Uncurried (EffectFn4, mkEffectFn4)
 import Perspectives.Couchdb (DeleteCouchdbDocument)
 import Perspectives.Couchdb.Databases (addAttachment) as PCD
 import Perspectives.CouchdbState (runMonadCouchdb)
 import Prelude (($))
 
-addAttachment :: String -> String -> String -> MediaType -> Effect (Promise DeleteCouchdbDocument)
-addAttachment docPath attachmentName attachment mimetype = fromAff $
+addAttachment_ :: String -> String -> String -> MediaType -> Effect (Promise DeleteCouchdbDocument)
+addAttachment_ docPath attachmentName attachment mimetype = fromAff $
   runMonadCouchdb "admin" "admin"
     (PCD.addAttachment docPath attachmentName attachment mimetype)
+
+addAttachment :: EffectFn4 String String String MediaType (Promise DeleteCouchdbDocument)
+addAttachment = mkEffectFn4 addAttachment_
