11fe6afde29ddca260b029f5b108a2c32029e224
Joop Ringelberg <joop.ringelberg@gmail.com>
2019-06-13 12:02:23 +0200

    Distribution with affjax that handles webpack

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # purescript-couchdb
 
 Functions to log into Couchdb, manage users and databases.
+
+## Dependency
+This library uses Affjax. From the documentation of that library:
+
+If you are intending to use the library in a Node.js setting rather than the browser, you will need an additional dependency from npm:
+
+```
+npm install xhr2
+```
+However, instead one should use a version of that library that handles cookies:
+
+```
+npm install xhr2-cookies
+```
+So make sure to add that dependency when you use purescript-couchdb in a node setting.
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+  entry: path.join(__dirname, "output/Main/index.js" ),
+  output: {
+    library: "perspectives-couchdb",
+    libraryTarget: "commonjs2",
+    filename: "perspectives-couchdb.js",
+    path: path.join(__dirname, "dist")
+  },
+  watch: false,
+  mode: "development",
+  target: "node",
+  module: {
+    rules: []
+  },
+  externals: {
+    "perspectives-proxy": {
+      commonjs: 'perspectives-proxy',
+      commonjs2: 'perspectives-proxy',
+      amd: 'perspectives-proxy',
+      root: "perspectivesProxy"
+    }
+  }
+};
