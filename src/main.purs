84d80b3005a153acd34989f562885c7750a896bd
Joop Ringelberg <joop.ringelberg@gmail.com>
2019-06-13 08:42:36 +0200

    All arguments to addAttachment now are strings

--- src/main.purs
+++ src/main.purs
@@ -1,7 +1,7 @@
 module Main (addAttachment) where
 
 import Control.Promise (Promise, fromAff)
-import Data.MediaType (MediaType)
+import Data.MediaType (MediaType(..))
 import Effect (Effect)
 import Effect.Uncurried (EffectFn4, mkEffectFn4)
 import Perspectives.Couchdb (DeleteCouchdbDocument)
@@ -9,10 +9,10 @@ import Perspectives.Couchdb.Databases (addAttachment) as PCD
 import Perspectives.CouchdbState (runMonadCouchdb)
 import Prelude (($))
 
-addAttachment_ :: String -> String -> String -> MediaType -> Effect (Promise DeleteCouchdbDocument)
+addAttachment_ :: String -> String -> String -> String -> Effect (Promise DeleteCouchdbDocument)
 addAttachment_ docPath attachmentName attachment mimetype = fromAff $
   runMonadCouchdb "admin" "admin"
-    (PCD.addAttachment docPath attachmentName attachment mimetype)
+    (PCD.addAttachment docPath attachmentName attachment (MediaType mimetype))
 
-addAttachment :: EffectFn4 String String String MediaType (Promise DeleteCouchdbDocument)
+addAttachment :: EffectFn4 String String String String (Promise DeleteCouchdbDocument)
 addAttachment = mkEffectFn4 addAttachment_
